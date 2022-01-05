b42d77d5e3a9d9894930bee4490727510ca1ce9a
ivukic99 <igor.vukic8877@gmail.com>
2022-01-05 23:27:55 +0100

    added dynamic list of users and users search

--- index.php
+++ index.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ChetApp by Igor</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
-
+<?php include_once "header.php" ?>
 <body>
     <div class="wrapper">
         <section class="form singup">
             <header>
-                Realtime Chat app
+                Chat app
             </header>
             <form action="#" enctype="multipart/form-data" autocomplete="off">
                 <div class="error-txt">
