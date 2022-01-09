e1fcf2f8e7035d78b55774fcd4930192e16aee0e
ivukic99 <igor.vukic8877@gmail.com>
2022-01-10 00:13:21 +0100

    logout function and small update on users list

--- javascript/chat.js
+++ javascript/chat.js
@@ -13,11 +13,19 @@ sendBtn.onclick = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 inputField.value = "";
+                scrollToBottom();
             }
         }
     }
     let formData = new FormData(form); //creating new FormData Object
     xhr.send(formData); //sending the form data to php
+}
+
+chatBox.onmouseenter = () => {
+    chatBox.classList.add('active');
+}
+chatBox.onmouseleave = () => {
+    chatBox.classList.remove('active');
 }
 
 setInterval(() => {
@@ -28,9 +36,17 @@ setInterval(() => {
             if (xhr.status === 200) {
                 let data = xhr.response;
                 chatBox.innerHTML = data;
+                if (!chatBox.classList.contains("active")) {
+                    scrollToBottom();
+                }
             }
         }
     }
     let formData = new FormData(form); //creating new FormData Object
     xhr.send(formData);
 }, 500)
+
+
+scrollToBottom = () => {
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
